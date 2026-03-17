4681eab93794a04a1ea536d3df3a405b7e15b62e
Djellal Elkacem <djellal.elkacem@gmail.com>
2026-03-17 12:20:45 +0100

    refactor: Reimplement email sending functionality using the MailKit library and update the sender email address.

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,5 +1,6 @@
-using System.Net;
-using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
 
 namespace BarideWeb.Services
 {
@@ -27,22 +28,21 @@ namespace BarideWeb.Services
             var fromEmail = smtp["FromEmail"]!;
             var fromName = smtp["FromName"] ?? "BaridUFAS";
 
-            using var client = new SmtpClient(host, port)
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(fromName, fromEmail));
+            message.To.Add(new MailboxAddress(toName, toEmail));
+            message.Subject = subject;
+
+            message.Body = new TextPart("html")
             {
-                Credentials = new NetworkCredential(username, password),
-                EnableSsl = true
+                Text = htmlBody
             };
 
-            var message = new MailMessage
-            {
-                From = new MailAddress(fromEmail, fromName),
-                Subject = subject,
-                Body = htmlBody,
-                IsBodyHtml = true
-            };
-            message.To.Add(new MailAddress(toEmail, toName));
-
-            await client.SendMailAsync(message);
+            using var client = new SmtpClient();
+            await client.ConnectAsync(host, port, SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(username, password);
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
         }
     }
 }
